9ec39e8513f9481870af47fa081c240115a48503
Levi <115052769+leviobrabo@users.noreply.github.com>
2024-12-26 18:53:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,11 @@ session = requests.Session()
 
 # Define um User-Agent para evitar bloqueios de requisições automatizadas
 session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+    'Referer': LOGIN_URL,
+    'Origin': LOGIN_URL.split('/login')[0]  # Ajuste conforme necessário
 })
+
 
 # Configura o MongoDB
 client = MongoClient(MONGO_URI)
@@ -69,7 +72,7 @@ def login() -> bool:
     logging.info("Tentando realizar login...")
     try:
         response = session.get(LOGIN_URL)
-        print(response.status_code)
+        print(f"Status Code: {response.status_code}")
         print(response.text)
         logging.info(f"Resposta GET LOGIN_URL: {response.status_code}")
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -79,6 +82,7 @@ def login() -> bool:
 
         if not form_build_id or not form_id:
             logging.error("Campos do formulário de login não encontrados. Verifique a página de login.")
+            print(response.text)
             return False
 
         payload = {
@@ -88,10 +92,12 @@ def login() -> bool:
             'form_id': form_id['value'],
             'op': 'Entrar'
         }
-
+        print(payload)
         logging.info(f"Payload de login: {payload}")
         login_response = session.post(LOGIN_URL, data=payload)
         logging.info(f"Resposta POST LOGIN_URL: {login_response.status_code}")
+        print(f"Status Code POST: {login_response.status_code}")
+        print(login_response.text)
 
         if "Sair" in login_response.text:
             logging.info("Login realizado com sucesso.")
